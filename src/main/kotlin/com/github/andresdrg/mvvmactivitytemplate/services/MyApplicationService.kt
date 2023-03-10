b2c469949c8708310ec25b1394e1a495a9dd1104
GitHub Action <action@github.com>
2023-03-10 09:04:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/andresdrg/mvvmactivitytemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/andresdrg/mvvmactivitytemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.andresdrg.mvvmactivitytemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.andresdrg.mvvmactivitytemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/andresdrg/mvvmactivitytemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/andresdrg/mvvmactivitytemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.andresdrg.mvvmactivitytemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.andresdrg.mvvmactivitytemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
